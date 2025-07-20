fe420bdf942d1eda1b80bf96dad7bb7077bc22a3
Vladimir <willfordrockwell@gmail.com>
2025-07-20 22:50:25 +0800

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class Main {
-    public static void main(String[] args) {
-        List<Employee> employees = XMLParser.parseXML("data.xml");
-        JsonWriter.listToJson(employees, "data2.json");
-        System.out.println("Файл data2.json успешно создан.");
-    }
-}
